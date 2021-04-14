f9d0363ba20d247066dabaf445dcc7e9e34e5b61
Mason <masonbush751@gmail.com>
2021-04-14 15:15:21 -0500

    Frontend baseline

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const routesReport = require('rowdy-logger').begin(app)
+const path = require('path')
+
+//const replaceInFile = require('replace-in-file')
+
+app.get('/', (req, res) => {
+  const filepath = path.join(__dirname, 'index.html')
+  res.sendFile(filepath)
+})
+
+app.get('/main.js', async (req, res) => {
+  const filepath = path.join(__dirname, 'main.js')
+
+//   if (process.env.NODE_ENV === 'production') {
+//     await replaceInFile({
+//       files: filepath,
+//       from: 'http://localhost:3001/',
+//       to: 'https://wargame-backend.herokuapp.com/' 
+//     })
+//   }
+
+  res.sendFile(filepath)
+})
+
+app.get('/style.css', (req, res) => {
+  const filepath = path.join(__dirname, 'style.css')
+  res.type('css').sendFile(filepath)
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    routesReport.print()
+})
