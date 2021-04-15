864fd92dbde18ea755a9b3241024596adfac4267
Mason <masonbush751@gmail.com>
2021-04-14 21:10:59 -0500

    login

--- main.js
+++ main.js
@@ -4,9 +4,11 @@ const url = 'http://localhost:3001/'
 
 const homepage = document.querySelector('.homepage')
 const loginForm = document.querySelector('.loginForm')
+const loginButton = document.querySelector('.login')
 const signUpForm = document.querySelector('.signUpForm')
-
+const signUpButton = document.querySelector('.signUp')
 const signOut = document.querySelector('.signOut')
+const addBizButton = document.querySelector('.addBiz')
 
 
 // Sign Up Form
@@ -15,6 +17,7 @@ document.querySelector('.signUp').addEventListener('click', () => {
 })
 
 signUpForm.addEventListener('submit', async (event) => {
+    event.preventDefault()
     const name = document.querySelector('.signUpName').value
     const email = document.querySelector('.signUpEmail').value
     const password = document.querySelector('.signUpPassword').value
@@ -42,13 +45,12 @@ document.querySelector('.login').addEventListener('click', () => {
 })
 
 loginForm.addEventListener('submit', async (event) => {
-    const name = document.querySelector('.loginName').value
+    event.preventDefault()
+    const modal = document.querySelector('.loginModal')
     const email = document.querySelector('.loginEmail').value
-    const password = document.querySelector(',loginPassword').value
-
+    const password = document.querySelector('.loginPassword').value
     try {
         const response = await axios.post(`${url}users/login`, {
-            name: name,
             email: email,
             password: password
         })
@@ -56,7 +58,12 @@ loginForm.addEventListener('submit', async (event) => {
         const userId = response.data.user.id
         localStorage.setItem('userId', userId)
 
-    }catch (error) {
+        modal.style.display = 'none'
+        loginButton.classList.add('hidden')
+        signUpButton.classList.add('hidden')
+        addBizButton.classList.remove('hidden')
+        signOut.classList.remove('hidden')
+    } catch (error) {
         alert('login failed')
     }
 })
@@ -64,6 +71,7 @@ loginForm.addEventListener('submit', async (event) => {
 signOut.addEventListener('click', () => {
     console.log('you logged out');
     localStorage.removeItem('userId')
+    location.reload()
 })
 
 document.querySelector('.addBiz').addEventListener('click', () => {
@@ -91,7 +99,7 @@ const triggerModal = (querySelect, closePos) => {
     }
 }
 
-/*If (userloggedin) {
+if (localStorage.getItem('userId')) {
     document.querySelector('.login').classList.add('hidden')
     document.querySelector('.signUp').classList.add('hidden')
     document.querySelector('.addBiz').classList.remove('hidden')
@@ -102,4 +110,3 @@ const triggerModal = (querySelect, closePos) => {
     document.querySelector('.addBiz').classList.add('hidden')
     document.querySelector('.signOut').classList.add('hidden')
 }
-*/
